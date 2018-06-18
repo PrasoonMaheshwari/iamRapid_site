1ea21bae4adfed601e511e2a4acef61c5e313564
Swastik Singh <swastik.singh0301@gmail.com>
2018-06-18 06:15:19 +0000

    mongo introduced

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var fileUpload = require("express-fileupload");
 var unzip = require("unzip-stream");
 const methodOverride = require('method-override');
 const editJson = require("edit-json-file");
+var mongoose = require("mongoose");
 
 
 app.use(methodOverride('_method'));
@@ -16,37 +17,73 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(fileUpload());
 app.use(express.static("public"));
 
+
+mongoose.connect("mongodb://localhost/rapid");
+
+var rapidSchema = new mongoose.Schema({
+   name: String,
+   printTime: String,
+   weight: String
+});
+
+
+var Rapid = mongoose.model("Rapid", rapidSchema);
+
 var sampleFileName;
 
 
 app.get("/",function(req, res) {
-     var dataTime = fs.readFileSync("outputTime.txt", "utf8");
-     var dataWeight = fs.readFileSync("outputWeight.txt", "utf8");
-     fs.readFile('public/uploadFiles.json', 'utf8', function readFileCallback(err, data){
-        var obj = JSON.parse(data); //now it an object
-        if(sampleFileName)
-        {
-            if (err){
-                console.log(err);
-            } else {
-                var flag =0;
-                obj.users.forEach(function(temp){
-                    if(temp.name === sampleFileName)
-                    {
-                        temp.printTime=dataTime;
-                        temp.printWeight=dataWeight;
-                        flag=1;
-                    }
-                });
-                if(flag===0)
-                {
-                    obj.users.push({ "name": sampleFileName , "printTime" : dataTime , "printWeight" : dataWeight}); //add some data
-                }
-                var json = JSON.stringify(obj); //convert it back to json
-                fs.writeFile('public/uploadFiles.json', json, 'utf8'); // write it back 
-            }
-        }
-        res.render("new", {obj:obj});
+    //  var dataTime = fs.readFileSync("outputTime.txt", "utf8");
+    //  var dataWeight = fs.readFileSync("outputWeight.txt", "utf8");
+     
+     
+     
+    //  commented out part, because it was related to uploadFiles.json
+    //  fs.readFile('public/uploadFiles.json', 'utf8', function readFileCallback(err, data){
+    //     var obj = JSON.parse(data); //now it an object
+    //     if(sampleFileName)
+    //     {
+    //         if (err){
+    //             console.log(err);
+    //         } else {
+    //             var flag =0;
+    //             obj.users.forEach(function(temp){
+    //                 if(temp.name === sampleFileName)
+    //                 {
+    //                     temp.printTime=dataTime;
+    //                     temp.printWeight=dataWeight;
+    //                     flag=1;
+    //                 }
+    //             });
+    //             if(flag===0)
+    //             {
+    //                 obj.users.push({ "name": sampleFileName , "printTime" : dataTime , "printWeight" : dataWeight}); //add some data
+    //             }
+    //             var json = JSON.stringify(obj); //convert it back to json
+    //             fs.writeFile('public/uploadFiles.json', json, 'utf8'); // write it back 
+    //         }
+    //     }
+    //     res.render("new", {obj:obj});
+    // });
+    
+    
+    // converting the above code for mongodb
+    // if(sampleFileName){
+    //     Rapid.findOne({name : sampleFileName}, function(err, founddata){
+    //       if(founddata){
+    //       founddata.printTime = dataTime;
+    //       founddata.weight = dataWeight;
+    //   }
+    //     });
+    // }
+    
+    Rapid.find({}, function(err, obj){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("new",{obj:obj});
+          //console.log (obj[0]._id);
+       }
     });
 });    
 
@@ -60,9 +97,23 @@ app.post('/upload', (req, res) => {
     let sampleFile = req.files.sampleFile;
     //console.log(sampleFile);
     sampleFileName = req.files.sampleFile.name;
+    
+    // var dataTime = fs.readFileSync("outputTime.txt", "utf8");
+    // var dataWeight = fs.readFileSync("outputWeight.txt", "utf8");
+    
+    // var newRapid = {name: sampleFileName, printTime: dataTime, weight: dataWeight};
+    // Rapid.create(newRapid, function(err, newlyCreated){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     else {
+    //         console.log(newlyCreated);
+    //     }
+    // });
+    
     // Use the mv() method to place the file somewhere on your server
     
-    sampleFile.mv('uploads/'+ sampleFileName, function(err) {
+    sampleFile.mv('public/uploads/'+ sampleFileName, function(err) {
         if (err)
           return res.status(500).send(err);
         if(sampleFile.mimetype ==='application/octet-stream')
@@ -73,22 +124,41 @@ app.post('/upload', (req, res) => {
             file.get().categories.resolution.settings.layer_height.default = 0.2;
             file.get().categories.infill.settings.infill_sparse_density.children.infill_line_distance.default = 2.333;
             file.save();
-            cmd.get(
-                 cmdURL
-                 ,function(err, data, stderr){
-                     if (!err) {
-                       //console.log('terminal runs');
-                        // res.redirect("/");
-                     } else {
-                       console.log('error', err);
-                     }
-                 }
-            );
+            // cmd.get(
+            //      cmdURL
+            //      ,function(err, data, stderr){
+            //          if (!err) {
+            //           //console.log('terminal runs');
+            //             // res.redirect("/");
+            //          } else {
+            //           console.log('error', err);
+            //          }
+            //      }
+                 
+            // );
+            cmd.get(cmdURL, function (){
+                var dataTime = fs.readFileSync("outputTime.txt", "utf8");
+                var dataWeight = fs.readFileSync("outputWeight.txt", "utf8");
+                console.log(dataTime);
+                console.log(dataWeight);
+    
+                var newRapid = {name: sampleFileName, printTime: dataTime, weight: dataWeight};
+                Rapid.create(newRapid, function(err, newlyCreated){
+                    if(err){
+                        console.log(err);
+                    }
+                    else {
+                        //console.log(newlyCreated);
+                    }
+                });
+                res.redirect("/");
+            });
+ 
         }
         else if(sampleFile.mimetype ==='application/x-zip-compressed')
         {
             //unzipping
-            fs.createReadStream("uploads/"+sampleFileName)
+            fs.createReadStream("public/uploads/"+sampleFileName)
             .pipe(unzip.Parse())
             .on('entry', function (entry) {
                  var filePath = entry.path;
@@ -101,31 +171,61 @@ app.post('/upload', (req, res) => {
                         console.log("Choco");
                         sampleFileName=fileName;
                         console.log(sampleFileName);
-                        entry.pipe(fs.createWriteStream("uploads/"+fileName));
+                        entry.pipe(fs.createWriteStream("public/uploads/"+fileName));
                         
                         console.log("Image Start");
-                        stltoimg();
-                        console.log("Image End");
-                        console.log("Cura Start");
-                        var cmdURL = commandURL();
-                        let file = editJson("Cura/resources/machines/fdmprinter.json");
-                        file.get().categories.resolution.settings.layer_height.default = 0.2;
-                        file.get().categories.infill.settings.infill_sparse_density.children.infill_line_distance.default = 2.333;
-                        file.save();
-                        cmd.get(
-                            cmdURL
-                          ,function(err, data, stderr){
-                            if (!err) {
+                        //stltoimg(()=>{
+                            var thumbnailer = new StlThumbnailer({
+        //url: req.query.url,           // url OR filePath must be supplied, but not both
+        filePath: "public/uploads/" + sampleFileName,            // load file from filesystem
+        requestThumbnails: [
+            {
+                width: 350,
+                height: 350,
+            }
+        ]   
+    })
+    .then(function(thumbnails){
+          // thumbnails is an array (in matching order to your requests) of Canvas objects
+          // you can write them to disk, return them to web users, etc
+          thumbnails[0].toBuffer(function(err, buf){      
+          //res.contentType('image/png');
+          //console.log(typeof buf);
+          var createStream = fs.createWriteStream("public/" + sampleFileName + ".jpg");
+          createStream.end();
+          fs.writeFileSync("public/"+ sampleFileName + ".jpg", buf);
+        })
+    });
+                            console.log("Image End");
+                            console.log("Cura Start");
+                            var cmdURL = commandURL();
+                            let file = editJson("Cura/resources/machines/fdmprinter.json");
+                            file.get().categories.resolution.settings.layer_height.default = 0.2;
+                            file.get().categories.infill.settings.infill_sparse_density.children.infill_line_distance.default = 2.333;
+                            file.save();
+                            cmd.get(cmdURL, function(err){
+                                if (!err) {
                                     console.log("Cura End");
-                                    //console.log('terminal runs');
-                                    console.log("JSON upload start");
-                                    uploadJSON();
-                                    console.log("JSON upload end");
-                              } else {
+                                    var dataTime = fs.readFileSync("outputTime.txt", "utf8");
+                                    var dataWeight = fs.readFileSync("outputWeight.txt", "utf8");
+                                    console.log(dataTime);
+                                    console.log(dataWeight);
+    
+                                    var newRapid = {name: sampleFileName, printTime: dataTime, weight: dataWeight};
+                                    Rapid.create(newRapid, function(err, newlyCreated){
+                                        if(err){
+                                            console.log(err);
+                                        }
+                                        else {
+                                            console.log(newlyCreated);
+                                        }
+                                    });
+                                } else {
                                     console.log('error', err);
                                 }
                             }
                         );
+                        
                         
         
                   } 
@@ -133,14 +233,30 @@ app.post('/upload', (req, res) => {
                         entry.autodrain();
                   }
             });
+            res.redirect("/");
         }
         else{
             console.log("Enter .stl file");
             sampleFileName = '';
             res.redirect("/");
         }
-        res.redirect("/");
+        //res.redirect("/");
     });
+    // var dataTime = fs.readFileSync("outputTime.txt", "utf8");
+    // var dataWeight = fs.readFileSync("outputWeight.txt", "utf8");
+    // console.log(dataTime);
+    // console.log(dataWeight);
+    
+    // var newRapid = {name: sampleFileName, printTime: dataTime, weight: dataWeight};
+    // Rapid.create(newRapid, function(err, newlyCreated){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     else {
+    //         console.log(newlyCreated);
+    //     }
+    // });
+    // res.redirect("/");
 });
 
 app.post("/modify",function(req,res){
@@ -174,43 +290,38 @@ app.post("/modify",function(req,res){
 });
 
 
+// delete route
 app.post("/delete/:id",function(req, res) {
-    var deleteName = req.body.delete;
-    fs.readFile('public/uploadFiles.json', 'utf8', function readFileCallback(err, data){
-        var obj = JSON.parse(data); //now it an object
-        //console.log(obj.users.name);
-        if(deleteName)
-        {
-            if (err){
-                console.log(err);
-            } else {
-                var number=0;
-                var i=0;
-                obj.users.forEach(function(temp){
-                    if(temp.name === deleteName)
-                    {
-                        number = i;
-                    }
-                    else{
-                        i++;
-                    }
-                });
-                delete obj.users[number];
-                var json = JSON.stringify(obj); //convert it back to json
-                fs.writeFile('public/uploadFiles.json', json, 'utf8'); // write it back 
-            }
-        }
+    Rapid.findByIdAndRemove(req.params.id, function (err){
         res.redirect("/");
-    });    
+    });
 });
 
 
 
+// function to run command in terminal
 function commandURL(){
-    return 'cd CuraEngine-2.1.3 && \ ./build/CuraEngine slice -v -j ../Cura/resources/machines/dual_extrusion_printer.json -o "output/test.gcode" -e1 -s infill_line_distance=0 -e0 -l "../uploads/' + sampleFileName + '"';
+    return 'cd CuraEngine && \ ./build/CuraEngine slice -v -j ../Cura/resources/machines/dual_extrusion_printer.json -o "output/test.gcode" -e1 -s infill_line_distance=0 -e0 -l "../public/uploads/' + sampleFileName + '"';
 };
 
 
+
+// function to create new data of a new stl file
+function uploadtodb(newd){
+    Rapid.create(newd, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }
+    });
+}
+
+
+
+// function to modify existing data of a stl file
+
+
+
+// function to upload a JSON file
 function uploadJSON(){
     
     var dataTime = fs.readFileSync("outputTime.txt", "utf8");
@@ -243,10 +354,13 @@ function uploadJSON(){
     
 }    
 
+
+
+// function to get image of stl file
 function stltoimg(){
     var thumbnailer = new StlThumbnailer({
         //url: req.query.url,           // url OR filePath must be supplied, but not both
-        filePath: "uploads/" + sampleFileName,            // load file from filesystem
+        filePath: "public/uploads/" + sampleFileName,            // load file from filesystem
         requestThumbnails: [
             {
                 width: 350,
